ce2e7c12cb0275db8d14e692e913cf4c76974cbd
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-16 20:17:24 +0545

    Task model fillable

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'project_id',
+        'user_id',
+        'days',
+        'hours',
+        'company_id',
+    ];
 }
