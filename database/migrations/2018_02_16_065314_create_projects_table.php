842b2a69e6de6f59af613f37f8be0c20c4d153d9
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-16 12:42:51 +0545

    added name, desc, company_id, user_id in projects

--- database/migrations/2018_02_16_065314_create_projects_table.php
+++ database/migrations/2018_02_16_065314_create_projects_table.php
@@ -15,6 +15,10 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('company_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
         });
     }
