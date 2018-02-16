366178ff40f880c5ce845f99b9a92645523f7688
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-16 20:06:56 +0545

    Project model fillable

--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'description',
+        'company_id',
+        'user_id',
+        'days',
+    ];
 }
