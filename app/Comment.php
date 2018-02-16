ac44b6d7c288fb290f63ac4cb68ab287004f5a87
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-16 20:10:31 +0545

    Comment model fillable

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+     protected $fillable = [
+        'body',
+        'url',
+        'user_id',
+        'commentable_id',
+        'commentable_type',
+    ];
 }
