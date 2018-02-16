c614f14c8362a92a78ef5bccb9b120205d1d93aa
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-16 14:54:10 +0545

    php artisan make:migration create_task_user_table

--- database/migrations/2018_02_16_090308_create_task_user_table.php
+++ database/migrations/2018_02_16_090308_create_task_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectUserTable extends Migration
+class CreateTaskUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateProjectUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('project_user', function (Blueprint $table) {
+        Schema::create('task_user', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->integer('project_id')->unsigned();
+            $table->integer('task_id')->unsigned();
             $table->integer('user_id')->unsigned();
 
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('task_id')->references('id')->on('tasks');
 
             $table->timestamps();
         });
@@ -33,6 +33,6 @@ class CreateProjectUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('project_user');
+        Schema::dropIfExists('task_user');
     }
 }
