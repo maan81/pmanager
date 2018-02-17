82a5b0cb563c140bce0faf55c94bc3e6bed6d705
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-17 13:59:20 +0545

    extending & using layouts in listing companies

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -1,17 +1,23 @@
-<div class="panel panel-default">
+@extends('layouts.app')
 
-    <div class="panel-heading">Panel heading without title</div>
+@section('content')
 
-    <div class="panel-body">
+    <div class="panel panel-default">
 
-    <ul class="list-group">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Morbi leo risus</li>
-        <li class="list-group-item">Porta ac consectetur ac</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-    </ul>
+        <div class="panel-heading">Panel heading without title</div>
 
-  </div>
+        <div class="panel-body">
 
-</div>
+        <ul class="list-group">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+
+      </div>
+
+    </div>
+
+@endsection
