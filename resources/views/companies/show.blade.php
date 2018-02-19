f046b42ef833aa917597a00897bde9cd6f5736a1
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-19 11:47:12 +0545

    adjusting view layout

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -2,8 +2,51 @@
 
 @section('content')
 
+    <div class="col-md-9">
+        <!-- <pre>{{print_r($company)}}</pre> -->
 
-    <div class="blog-sidebar col-md-3 pull-right">
+        <!-- <pre>{{print_r($company->projects)}}</pre> -->
+
+        <!-- Jumbotron -->
+        <div class="jumbotron">
+            <h1>{{$company->name}}!</h1>
+            <p class="lead">{{$company->description}}</p>
+            <!-- <p>
+                <a class="btn btn-lg btn-success" href="#" role="button">Get started today</a>
+            </p> -->
+        </div>
+
+        <!-- Example row of columns -->
+        <div class="row">
+            @foreach($company->projects as $project)
+                <div class="col-lg-4">
+                    <h2>{{$project->name}}</h2>
+                    <!-- <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p> -->
+                    <p>{{$project->description}}</p>
+                    <p>
+                        <a class="btn btn-primary" href="/projects/{{$project->id}}" role="button">View details &raquo;</a>
+                    </p>
+                </div>
+                <!-- <div class="col-lg-4">
+                    <h2>Heading</h2>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <p>
+                        <a class="btn btn-primary" href="#" role="button">View details &raquo;</a>
+                    </p>
+                </div>
+                <div class="col-lg-4">
+                    <h2>Heading</h2>
+                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                    <p>
+                        <a class="btn btn-primary" href="#" role="button">View details &raquo;</a>
+                    </p>
+                </div> -->
+            @endforeach
+        </div>
+
+    </div>
+
+    <div class="blog-sidebar col-md-3">
         <div class="sidebar-module">
             <h4>Actions</h4>
             <ol class="list-unstyled">
@@ -48,50 +91,5 @@
             </ol>
         </div>
     </div><!-- /.blog-sidebar -->
-
-
-    <div class="col-md-9">
-        <!-- <pre>{{print_r($company)}}</pre> -->
-
-        <!-- <pre>{{print_r($company->projects)}}</pre> -->
-
-        <!-- Jumbotron -->
-        <div class="jumbotron">
-            <h1>{{$company->name}}!</h1>
-            <p class="lead">{{$company->description}}</p>
-            <!-- <p>
-                <a class="btn btn-lg btn-success" href="#" role="button">Get started today</a>
-            </p> -->
-        </div>
-
-        <!-- Example row of columns -->
-        <div class="row">
-            @foreach($company->projects as $project)
-                <div class="col-lg-4">
-                    <h2>{{$project->name}}</h2>
-                    <!-- <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p> -->
-                    <p>{{$project->description}}</p>
-                    <p>
-                        <a class="btn btn-primary" href="/projects/{{$project->id}}" role="button">View details &raquo;</a>
-                    </p>
-                </div>
-                <!-- <div class="col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p>
-                        <a class="btn btn-primary" href="#" role="button">View details &raquo;</a>
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                    <p>
-                        <a class="btn btn-primary" href="#" role="button">View details &raquo;</a>
-                    </p>
-                </div> -->
-            @endforeach
-        </div>
-
-    </div>
 
 @endsection
