6b832ec4686a6e471509d145708422a0c9f2e511
Pranij Man Shrestha <pranijman@gmail.com>
2018-02-22 10:56:54 +0545

    41 - projects controller. need to be cleaned up

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -46,6 +46,14 @@ class ProjectsController extends Controller
     public function create($id = null)
     {
         $this->get_company($id);
+
+        dd($id);
+        dd($company);
+        dd($this->company);
+        // dd('create project');
+
+
+        return view('projects.create',['company'=>$this->company]);
     }
 
     /**
@@ -57,6 +65,17 @@ class ProjectsController extends Controller
     public function store(Request $request, $id = null)
     {
         $this->get_company($id);
+
+        $project = Project::create([
+            'name' => request('project-name'),
+            'description' => request('project-description'),
+            'user_id' => auth()->user()->id,
+            'company_id' => request('company-id'),
+        ]);
+
+        $project->save();
+
+        return redirect('projects/'.$project->id);
     }
 
     /**
@@ -68,6 +87,8 @@ class ProjectsController extends Controller
     public function show(Project $project, $id = null)
     {
         $this->get_company($id);
+
+        return view('projects.show',compact('project'));
     }
 
     /**
@@ -79,6 +100,8 @@ class ProjectsController extends Controller
     public function edit(Project $project, $id = null)
     {
         $this->get_company($id);
+
+        return view('projects.edit',compact('project'));
     }
 
     /**
@@ -91,6 +114,22 @@ class ProjectsController extends Controller
     public function update(Request $request, Project $project, $id = null)
     {
         $this->get_company($id);
+
+        $projectUpdate = Project::where('id',$project->id)
+            ->update([
+                'name' => $request->input('project-name'),
+                'description' => $request->input('project-description'),
+            ])
+        ;
+
+        if ($projectUpdate) {
+
+            return redirect('projects/'.$project->id)
+                ->with('success','Company Updated')
+            ;
+        }
+
+        return back()->withInput();
     }
 
     /**
@@ -102,5 +141,18 @@ class ProjectsController extends Controller
     public function destroy(Project $project, $id = null)
     {
         $this->get_company($id);
+
+        $id = $project->id;
+
+        if($project->delete()){
+            return redirect('projects')
+                ->with('success','Project ID '.$id.' deleted')
+            ;
+        }
+
+        return back()
+            ->withInput()
+            ->with('error', 'Project could not be deleted')
+        ;
     }
 }
