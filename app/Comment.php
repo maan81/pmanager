9241ed394f508822b3f65c003d75c6b059f412ed
Pranij Man Shrestha <pranijman@gmail.com>
2018-03-24 20:14:57 +0545

    44 - specifying polymorphic morphto relationships -- adding comments in the comment model

--- app/Comment.php
+++ app/Comment.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
-     protected $fillable = [
+    protected $fillable = [
         'body',
         'url',
         'user_id',
-        'commentable_id',
-        'commentable_type',
+        'commentable_id',   // project_id OR company_id
+        'commentable_type', // project OR company
     ];
 }
